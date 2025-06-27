a851f3c4729762eb39a59d7a03583b1461b59b19
Luca Cattani <luca.cattani@mail.polimi.it>
2025-06-27 12:11:49 +0200

    added logic for invalid tasks + some cleanup & comments

--- minimal-scheduler/sched_ext.bpf.c
+++ minimal-scheduler/sched_ext.bpf.c
@@ -3,7 +3,7 @@
 #include <bpf/bpf_tracing.h>
 #include <bpf/bpf_core_read.h>
 
-// Define a shared Dispatch Queue (DSQ) ID
+// Define shared Dispatch Queue (DSQ) IDs
 #define VE_DSQ_ID 12345
 #define VD_DSQ_ID 23456
 
@@ -14,7 +14,7 @@
     SEC("struct_ops.s/"#name)							      \
     BPF_PROG(name, ##args)
 
-// Define a BPF map to store virtual deadlines for tasks
+// Define a BPF map to store virtual eligible times for tasks
 struct {
     __uint(type, BPF_MAP_TYPE_HASH);
     __type(key, u32); // Task PID
@@ -22,6 +22,7 @@ struct {
     __uint(max_entries, 1024);
 } task_ves SEC(".maps");
 
+// Define a BPF map to store virtual deadlines for tasks
 struct {
     __uint(type, BPF_MAP_TYPE_HASH);
     __type(key, u32); // Task PID
@@ -29,10 +30,12 @@ struct {
     __uint(max_entries, 1024);
 } task_vds SEC(".maps");
 
-long vtime = 0; // still need to build the logic for updating vtime ~ WIP
+long vtime = 0;             // still need to build the logic for updating vtime ~ WIP
 long QUANTUMSIZE = 6000000; // default quantum size of 6 ms
 
-// returns the a task's virtual deadline
+/** 
+ * Returns the a task's virtual deadline
+ */
 static __always_inline u64 get_task_vd(struct task_struct *p) {
     u32 pid;
 
@@ -44,7 +47,9 @@ static __always_inline u64 get_task_vd(struct task_struct *p) {
     return vd_ptr ? *vd_ptr : -1;
 }
 
-// returns the a task's virtual deadline
+/**
+ * Returns the a task's virtual eligible time
+ */
 static __always_inline u64 get_task_ve(struct task_struct *p) {
     u32 pid;
 
@@ -56,32 +61,53 @@ static __always_inline u64 get_task_ve(struct task_struct *p) {
     return ve_ptr ? *ve_ptr : -1;
 }
 
-// Function to calculate the virtual deadline for a task
-static __always_inline u64 calculate_virtual_deadline(struct task_struct *p) {
+// Comptue and update a task's virtual deadline in the bpf map
+static __always_inline void update_virtual_deadline(struct task_struct *p) {
+    pid_t pid = p->pid;
+    
     u64 now = bpf_ktime_get_ns(); // Current time in nanoseconds
     u64 runtime = p->se.sum_exec_runtime; // Task's runtime
-    return now + runtime; // Example: deadline = now + runtime
+
+    u64 deadline = now + runtime;
+
+    bpf_map_update_elem(&task_vds, &pid, &deadline, BPF_ANY);
 }
 
-// Initialize the scheduler by creating a shared dispatch queue (DSQ)
+// Comptue and update a task's virtual eligible time in the bpf map
+static __always_inline void update_eligible_time(struct task_struct *p){
+    pid_t pid = p->pid;
+
+    u64 eligible = vtime;
+
+    bpf_map_update_elem(&task_ves, &pid, &eligible, BPF_ANY);
+}
+
+/**
+ * Initialize the scheduler by creating two shared dispatch queues (DSQ) ~ will be used as priority queues
+ * 
+ * VE_DSQ_ID contains tasks ordered by their virtual eligible time, all tasks here are not eligible
+ * VD_DSQ_ID contains tasks ordered by their virtual deadline, all tasks here are eligible
+ * 
+ * By default, all tasks are put in the VE DSQ. When tasks become eligible, they are moved to the VD DSQ for dispatch
+ */
 s32 BPF_STRUCT_OPS_SLEEPABLE(sched_init) {
     return scx_bpf_create_dsq(VE_DSQ_ID, -1) | scx_bpf_create_dsq(VD_DSQ_ID, -1);
 }
 
-// Enqueue a task to the shared DSQ, dispatching it with a virtual deadline
+/**
+ * Enqueue new tasks in the VE DSQ
+ * 
+ * Virtual deadline and eligible time are also computed and stored in the bpf maps
+ */
 int BPF_STRUCT_OPS(sched_enqueue, struct task_struct *p, u64 enq_flags) {
     u32 pid = p->pid;
-    u64 deadline = calculate_virtual_deadline(p);
-    u64 eligible = vtime;
 
-    // Store the virtual deadline in the BPF map
-    bpf_map_update_elem(&task_vds, &pid, &deadline, BPF_ANY);
-    
-    // Store the virtual deadline in the BPF map
-    bpf_map_update_elem(&task_ves, &pid, &eligible, BPF_ANY);
+    // Store the virtual deadline and eligible time in the BPF maps
+    update_virtual_deadline(p);
+    update_eligible_time(p);
 
     // Enqueue the task with on the DSQ, ordered by eligible time
-    scx_bpf_dsq_insert_vtime(p, VE_DSQ_ID, QUANTUMSIZE, eligible, enq_flags);
+    scx_bpf_dsq_insert_vtime(p, VE_DSQ_ID, QUANTUMSIZE, get_task_ve(p), enq_flags);
 
     return 0;
 }
@@ -120,7 +146,7 @@ static __always_inline bool is_task_allowed(struct task_struct *p, s32 cpu) {
 /**
  * Checks if the given task is eligible
  */
-static bool is_task_eligible(struct task_struct *p){
+static __always_inline bool is_task_eligible(struct task_struct *p){
     return get_task_ve(p) <= vtime;
 }
 
@@ -137,7 +163,7 @@ static void move_eligible_tasks_to_vd_dsq(s32 cpu){
         goto out;
     }
 
-    // iterate over the VE DSQ
+    // Iterate over the VE DSQ
     while ((p = bpf_iter_scx_dsq_next(&it__iter))) {
         if(is_task_eligible(p)){
             u64 deadline = get_task_vd(p);
@@ -150,7 +176,7 @@ static void move_eligible_tasks_to_vd_dsq(s32 cpu){
             
             bpf_printk("Successfully moved task %d to VD_DSQ, vd: %ld\n", p->pid, deadline);
         }
-        else break; // Since it's a priority queue, if this task is not eligible, others aren't either
+        else break; // Since it's a priority queue, if this task is not eligible, the next aren't either
     }
 
 out:
@@ -158,18 +184,53 @@ out:
     return;
 }
 
+/**
+ * Check if a task's deadline is expired (i.e., deadline has passed)
+ */
 static bool is_task_expired(struct task_struct *p){
     return get_task_vd(p) < vtime;
 }
 
 /**
- * Dispatch a task from the user-defined global DSQ (user-defined as a priority queue on vd) to the local cpu
+ * Move an invalid task from the VD DSQ to the VE DSQ
+ * 
+ * Vritual deadline and eligible time are also re-computed
+ */
+static void move_invalid_task_to_ve(struct task_struct *p){
+    struct bpf_iter_scx_dsq it__iter; // iterator over the VD DSQ
+    pid_t pid = p->pid;               // task pid
+    
+    // Initialize the DSQ iterator
+    if (bpf_iter_scx_dsq_new(&it__iter, VD_DSQ_ID, 0)){
+        bpf_printk("Failed to initialize VD DSQ iterator when re-scheduling task %d\n", pid);  
+        goto out;
+    }
+
+    // Update virtual deadline and eligible time in the BPF maps
+    update_virtual_deadline(p);
+    update_eligible_time(p);
+
+    // Move ask back to VE DSQ
+    scx_bpf_dsq_move_set_vtime(&it__iter, get_task_ve(p));
+    scx_bpf_dsq_move_vtime(&it__iter, p, VE_DSQ_ID, 0);
+
+    bpf_printk("Re-scheduled task %d due to expired deadline\n", pid);
+
+out:
+    bpf_iter_scx_dsq_destroy(&it__iter); // Destroy the DSQ iterator
+    return;
+}
+
+/**
+ * Move all eligible tasks from the VE to the VD DSQ, then iterate over the VD DSQ.
+ * Dispatch a task from the VD DSQ (user-defined as a priority queue on vd) to the local cpu
+ * 
  * Returns 1 if successful, 0 if no task was dispatched, negative error codes on failure
  */
 int BPF_STRUCT_OPS(sched_dispatch_dsq, s32 cpu, struct task_struct *prev) {
     struct bpf_iter_scx_dsq it__iter; // iterator over the VD DSQ
     struct task_struct *p = NULL;     // Pointer to the task being dispatched
-    int dispatched = 0;               // return value
+    int dispatched = 0;               // Return value
 
     // Check if any tasks became eligible
     move_eligible_tasks_to_vd_dsq(cpu);
@@ -188,6 +249,7 @@ int BPF_STRUCT_OPS(sched_dispatch_dsq, s32 cpu, struct task_struct *prev) {
 
         if(is_task_expired(p)){
             bpf_printk("Task %d has expired! vd: %ld vt: %ld\n", p->pid, get_task_vd(p), vtime);
+            move_invalid_task_to_ve(p);
             continue;
         }
 
@@ -201,7 +263,7 @@ int BPF_STRUCT_OPS(sched_dispatch_dsq, s32 cpu, struct task_struct *prev) {
     
 out:
     bpf_iter_scx_dsq_destroy(&it__iter); // Destroy the DSQ iterator
-    return dispatched; // No task was dispatched
+    return dispatched;
 }
 
 /*
